f3d2ff7e0190a8d98f81164bf3669054972bbd18
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2022-03-29 13:59:27 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+import asyncio
+from discord.ext import commands
+from serverstatus import *
+
+client = commands.Bot(command_prefix = "!", intents = discord.Intents.all())
+
+my_discord_channel_name = "<Insert Your Discord Channel Name Here>"
+
+@client.event
+async def on_ready(): 
+    while True:
+        category_list = []
+        server_status = getServerStatus(serverName)
+        availability = server_status.get("availability")
+        availability += " " + status_emoji[availability]
+        online_players = server_status.get("online-players")
+        
+        for server in client.guilds:
+            if server.name == my_discord_channel_name: # Add desired channels here               
+                for category in server.categories:
+                    if category.name == "Dry Tree":
+                        await category.move(beginning=True)
+                        category_list.append(category)
+                        for channel in category.channels:
+                            await channel.delete()
+                        await category.create_voice_channel("Status: " + availability)
+                        await category.create_voice_channel(online_players + " / 2,000")
+                        for channel in category.channels:
+                            for role in server.roles:
+                                await channel.set_permissions(role, connect=False) 
+                if len(category_list) == 0:
+                    try:
+                        print("Adding \"Dry Tree\" category to: " + server.name)
+                        await server.create_category("Dry Tree")
+                    except:
+                        print("Failed to add \"Dry Tree\" category to: " + server.name)
+                        continue
+        await asyncio.sleep(30)
+
+client.run(DISCORD_TOKEN)
