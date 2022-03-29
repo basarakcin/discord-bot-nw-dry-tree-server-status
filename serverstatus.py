56fb78fed04290d2669dfa4e448292eacc40b3c1
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2022-03-29 16:32:54 +0200

    Optimise serverstatus.py

--- serverstatus.py
+++ serverstatus.py
@@ -8,14 +8,17 @@ driver = webdriver.Chrome(options=opts)
 driver.get(URL)
 
 def getServerStatus(server_name):
-        data={}
         driver.get(URL)
+        data = []
         table = driver.find_element(By.TAG_NAME, 'tbody') 
         rows = table.find_elements(By.TAG_NAME, "tr")
         for row in rows:
                 txt = row.text
                 if server_name in txt:
                         raw_data = txt.split("\n")
-                        data['availability'] = raw_data[0]
-                        data['online-players'] = raw_data[2].split(" ", 5)[4] # This line MUST be edited for other servers
-        return data
+                        data.append(raw_data[0])
+                        data.append(raw_data[2].split(" ", 5)[4])
+                        print(data) # DEBUG
+        data.append("Status: " + data[0] + " " + status_emoji[data[0]])   
+        data.append(data[1] +  " / 2,000")             
+        return data[2:]
