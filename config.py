f95cf337edc4bb615e66a1c416048cb3ea03f0c9
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2022-03-29 14:09:14 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+DISCORD_TOKEN = r"<INSERT YOUR DISCORD TOKEN HERE>"
+URL = "https://nwdb.info/server-status"
+nw_server_name="Dry Tree" # Edit this variable for other servers
+status_emoji = dict(
+  Online = "✅",
+  Offline = "❌",
+  Maintenance = "⚠️",
+  )
+
+
+
